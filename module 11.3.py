573c48823cc645ef61df5b153a8adced0c1c8b6e
a-karaseva94 <a-karaseva94@mail.ru>
2024-09-22 13:16:46 +0300

    Add files via upload

--- module 11.3.py
+++ module 11.3.py
@@ -3,25 +3,18 @@ from pprint import pprint
 
 
 def introspection_info(obj):
-    info_about_object = {}
-    info_about_object['Тип объекта'] = type(obj).__name__
-    info_about_object['Атрибуты объекта'] = [attr for attr in dir(obj) if not attr.startswith("__")]
-    info_about_object['Методы объекта'] = [methods for methods in dir(obj) if methods.startswith("__")]
-    info_about_object['Модуль, к которому принадлежит объект'] = inspect.getmodule(obj)
-    info_about_object['Есть ли атрибут name у объекта'] = hasattr(obj, 'name')
-    info_about_object['Объект вызываемый?'] = callable(obj)
-    return info_about_object
+    info_about_obj = {'Тип объекта': type(obj),
+                      'Атрибуты объекта': [attr for attr in dir(obj) if
+                                           not callable(getattr(obj, attr)) and not attr.startswith("__")],
+                      'Методы объекта': [method for method in dir(obj) if
+                                         callable(getattr(obj, method)) and not method.startswith("__")],
+                      'Магические методы объекта': [method for method in dir(obj) if
+                                                    callable(getattr(obj, method)) and method.startswith("__")],
+                      'Модуль, к которому принадлежит объект': inspect.getmodule(obj),
+                      'Есть ли атрибут name у объекта': hasattr(obj, 'name'),
+                      'Объект вызываемый?': callable(obj)}
+    return info_about_obj
 
 
-class ClassInfo:
-    def __init__(self):
-        self.name = 'Nick'
-        self.years = 22
-
-
-obj = ClassInfo()
-number_info1 = introspection_info(obj)
-pprint(number_info1)
-
-number_info2 = introspection_info(42)
-pprint(number_info2)
+number_info = introspection_info(42)
+pprint(number_info)
