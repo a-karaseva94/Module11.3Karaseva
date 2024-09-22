2ab361975a84f84936ae8e97e92bdcb13edfc3e1
a-karaseva94 <a-karaseva94@mail.ru>
2024-09-22 11:54:51 +0300

    Add files via upload

--- module 11.3.py
+++ module 11.3.py
@@ -5,8 +5,8 @@ from pprint import pprint
 def introspection_info(obj):
     info_about_object = {}
     info_about_object['Тип объекта'] = type(obj).__name__
-    info_about_object['Атрибуты объекта'] = obj.__dict__
-    info_about_object['Методы и атрибуты объекта'] = dir(obj)
+    info_about_object['Атрибуты объекта'] = [attr for attr in dir(obj) if not attr.startswith("__")]
+    info_about_object['Методы объекта'] = [methods for methods in dir(obj) if methods.startswith("__")]
     info_about_object['Модуль, к которому принадлежит объект'] = inspect.getmodule(obj)
     info_about_object['Есть ли атрибут name у объекта'] = hasattr(obj, 'name')
     info_about_object['Объект вызываемый?'] = callable(obj)
@@ -20,5 +20,8 @@ class ClassInfo:
 
 
 obj = ClassInfo()
-number_info = introspection_info(obj)
-pprint(number_info)
+number_info1 = introspection_info(obj)
+pprint(number_info1)
+
+number_info2 = introspection_info(42)
+pprint(number_info2)
